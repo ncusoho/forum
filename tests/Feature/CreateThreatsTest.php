6c39137b63e7388509a78d2d8461f01395d0af92
yaorui <562247587@qq.com>
2018-07-27 16:25:05 +0000

    6.A User Can Publish Threads

---
+++ tests/Feature/CreateThreatsTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class CreateThreatsTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    public function testAnAuthenticatedUserCanCreateNewForumThreads()
+    {
+        $user = factory('App\User')->create();
+        $this->actingAs($user);
+        $thread = factory('App\Thread')->make(['user_id' => $user->id]);
+        $this->post('/threads', $thread->toArray());
+
+        $this->get($thread->path())
+            ->assertSee($thread->title)
+            ->assertSee($thread->body);
+    }
+
+    public function testGuestsMayNotCreateThreads()
+    {
+        $this->expectException('Illuminate\Auth\AuthenticationException');
+        $thread = factory('App\Thread')->make();
+        $this->post('/threads', $thread->toArray());
+    }
+}
