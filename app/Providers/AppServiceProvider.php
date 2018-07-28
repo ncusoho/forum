e706e66576be1b8b6ea5232614646c2068694a5e
yaorui <562247587@qq.com>
2018-07-28 04:50:53 +0000

    13.视图共享数据

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Carbon\Carbon;
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -15,6 +16,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Carbon::setLocale('zh');
+        View::share('channels', \App\Channel::all());
     }
 
     /**
