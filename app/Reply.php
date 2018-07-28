8adea7ad5cf3235e61a52bf76e1b4476766ba427
yaorui <562247587@qq.com>
2018-07-28 11:41:50 +0000

    18.对回复点赞

--- app/Reply.php
+++ app/Reply.php
@@ -13,4 +13,19 @@ class Reply extends Model
     {
         return $this->belongsTo('App\User', 'user_id');
     }
+
+    public function favorites()
+    {
+        return $this->morphMany('App\Favorite', 'favorited');
+    }
+
+    public function favorite()
+    {
+        $attributes = [
+            'user_id' => auth()->id(),
+        ];
+        if ( ! $this->favorites()->where($attributes)->exists()) {
+            $this->favorites()->create($attributes);
+        }
+    }
 }
--- readme.md
+++ readme.md
@@ -1,1 +1,3 @@
 laravel study
+
+<a href="https://laracasts.com/series/lets-build-a-forum-with-laravel">Let's Build A Forum with Laravel and TDD</a>
