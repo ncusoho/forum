ead97cb33d74a812fb917693be374a96c4e95729
yaorui <562247587@qq.com>
2018-07-28 04:34:51 +0000

    10.如何测试验证错误

--- tests/Feature/CreateThreatsTest.php
+++ tests/Feature/CreateThreatsTest.php
@@ -13,12 +13,14 @@ class CreateThreatsTest extends TestCase
 
     public function testAnAuthenticatedUserCanCreateNewForumThreads()
     {
-        $user = factory('App\User')->create();
-        $this->actingAs($user);
-        $thread = factory('App\Thread')->create(['user_id' => $user->id]);
-        $this->post('/threads', $thread->toArray());
+        $this->signIn();
 
-        $this->get($thread->path())
+        $thread = make('App\Thread');
+
+        $respones = $this->post('/threads', $thread->toArray());
+
+
+        $this->get($respones->headers->get('Location'))
             ->assertSee($thread->title)
             ->assertSee($thread->body);
     }
@@ -34,4 +36,37 @@ class CreateThreatsTest extends TestCase
         $this->get('/threads/create')
             ->assertRedirect('/login');
     }
+
+    public function publishThread(array $overrides = [])
+    {
+        $this->withExceptionHandling()->signIn();
+
+        $thread = make('App\Thread', $overrides);
+
+        return $this->post('/threads', $thread->toArray());
+    }
+
+    public function testAThreadRequiresATitle()
+    {
+        $this->publishThread(['title' => null])
+            ->assertSessionHasErrors('title');
+    }
+
+    public function testAThreadRequiresABody()
+    {
+        $this->publishThread(['body' => null])
+            ->assertSessionHasErrors('body');
+    }
+
+    public function testAThreadRequiresAValidChannel()
+    {
+        factory('App\Channel', 2)->create();
+
+        $this->publishThread(['channel_id' => null])
+            ->assertSessionHasErrors('channel_id');
+
+        $this->publishThread(['channel_id' => 999])
+            ->assertSessionHasErrors('channel_id');
+    }
+
 }
--- tests/Feature/ParticipateInForumTest.php
+++ tests/Feature/ParticipateInForumTest.php
@@ -33,4 +33,16 @@ class ParticipateInForumTest extends TestCase
 
     }
 
+    public function testAReplyRequiresABody()
+    {
+        $this->withExceptionHandling()->signIn();
+
+        $thread = create('App\Thread');
+        $reply = make('App\Reply', ['body' => null]);
+
+        $this->post($thread->path() . '/replies', $reply->toArray())
+            ->assertSessionHasErrors('body');
+
+    }
+
 }
